90f28825fe54dbb3e271fdf198f3b0aa05f47680
aartitalekar31 <aartitalekar31@gmail.com>
2024-08-05 23:43:44 +0530

    string with different functions
    string with different functions

---
+++ 03-String-Basics-Assign-01.js
@@ -0,0 +1,14 @@
+function stringBasics()
+{
+    console.log('my dream company name is:  " capgemini "'); 
+    var hobbies1="crafting";
+    var hobbies2=" reading";
+    var hobbies3=" mehandi-design";
+    console.log(`My Hobbies are:${hobbies1},${hobbies2},${hobbies3}`);
+    var result=hobbies1+hobbies2+hobbies3;
+    console.log("string after concatenation:",result);
+    
+    console.log("length of given string is:",result.length);
+    
+}
+stringBasics();
